52210c030900cdcf3bc7d1964021c3530cc65831
GaryPate <garyp@instance-1.australia-southeast1-b.c.ml-dev-403200.internal>
2023-11-08 02:30:55 +0000

    roll back dagster version

--- dagster_project/schedules.py
+++ dagster_project/schedules.py
@@ -1,4 +1,4 @@
-from dagster import ScheduleDefinition
+from dagster import ScheduleDefinition, DefaultScheduleStatus
 from dagster_project.jobs import sentimax_compute_job
 from dagster_dbt import build_schedule_from_dbt_selection
 from dagster_project.assets import get_dbt_assets
@@ -7,7 +7,8 @@ HOURLY_PLUS = " {} * * * * "
 
 sentimax_compute_schedule = ScheduleDefinition(job=sentimax_compute_job, 
                                                cron_schedule=HOURLY_PLUS.format('0'),
-                                               execution_timezone="Australia/Sydney")
+                                               execution_timezone="Australia/Sydney",
+                                               default_status=DefaultScheduleStatus.RUNNING)
 
 sentimax_dbt_assets_schedule = build_schedule_from_dbt_selection(
     [get_dbt_assets.dagster_dbt_assets],
