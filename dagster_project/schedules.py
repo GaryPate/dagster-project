cfbac379fc8aef5298a1de8dea02526338472e0d
GaryPate <garyp@instance-1.australia-southeast1-b.c.ml-dev-403200.internal>
2023-11-08 04:01:22 +0000

    schedule.py

--- dagster_project/schedules.py
+++ dagster_project/schedules.py
@@ -21,11 +21,11 @@ sentimax_compute_schedule = ScheduleDefinition(job=sentimax_compute_job,
 # selects all models tagged with "daily", and all their downstream asset dependencies
 sentimax_dbt_assets = build_dbt_asset_selection(
     [dagster_dbt_assets], 
-    dbt_select="tag:sentimax-dbt",
-    execution_timezone="Australia/Sydney"
+    dbt_select="tag:sentimax-dbt"
 ).downstream()
 
 sentimax_dbt_assets_schedule = ScheduleDefinition(
     job=define_asset_job("sentimax_dbt_assets", selection=sentimax_dbt_assets),
     cron_schedule=HOURLY_PLUS.format('7'),
+    execution_timezone="Australia/Sydney"
 )
